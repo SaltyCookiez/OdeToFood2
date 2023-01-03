d67311aa8c82c75b04942c0878d71e068a84515c
Moris Vider <morisvider@gmail.com>
2023-01-03 14:51:13 +0200

    add validation script

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,10 +1,20 @@
-﻿namespace OdeToFood.Models
+﻿using OdeToFood.Data.Migrations;
+using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Models
 {
     public class RestaurantReview
     {
         public int Id { get; set; }
+        [Range(1, 10)]
         public int Rating { get; set; }
+
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
+
+        [Display(Name = "User Name")]
+        [DisplayFormat(NullDisplayText = "Anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
     }
--- OdeToFood/Models/ViewModels/ReviewViewModel.cs
+++ OdeToFood/Models/ViewModels/ReviewViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace OdeToFood.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Models.ViewModels
 {
     public class ReviewViewModel
     {
         public int Id { get; set; }
+
+        [Range(1, 10)]
         public int Rating { get; set; }
+
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
     }
 }
