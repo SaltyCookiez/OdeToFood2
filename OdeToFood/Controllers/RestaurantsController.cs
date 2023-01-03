82bfc31f45d71d608655b437e914437fe16856a4
Moris Vider <morisvider@gmail.com>
2023-01-03 15:34:58 +0200

    Add Authorization to RestaurantsController

--- OdeToFood/Controllers/RestaurantsController.cs
+++ OdeToFood/Controllers/RestaurantsController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OdeToFood.Data;
 using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
+    [Authorize]
     public class RestaurantsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,6 +17,7 @@ namespace OdeToFood.Controllers
         }
 
         // GET: Restaurants
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
               return View(await _context.Restaurants.ToListAsync());
