028ee3eec40ea4f80f7a2fa8523ccc67d7cb23bc
Moris Vider <morisvider@gmail.com>
2023-01-03 14:21:53 +0200

    Add virutal and getting reviews by restaurant Id

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -7,6 +7,6 @@
         public string City { get; set; }
         public string Country { get; set; }
 
-        public ICollection<RestaurantReview> Reviews { get; set; }
+        public virtual ICollection<RestaurantReview> Reviews { get; set; }
     }
 }
